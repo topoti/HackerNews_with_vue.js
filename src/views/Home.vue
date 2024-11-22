166ff721581aea32421e8f199d03bcf4a9cc15a1
Topoti Sarkar <topoti@appscode.com>
2024-11-22 12:04:21 +0600

    initial commit
    Signed-off-by: Topoti Sarkar <topoti@appscode.com>

---
+++ src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event"/> 
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue';
+
+  export default {
+    name: 'Home',
+    components: {
+      EventCard
+    },
+
+    data() {
+      return{
+        events: [{
+          id: 532348,
+          category: 'animal welfare',
+          title: 'Cat Adoptation Day',
+          description: 'Find your new feline friend at this event',
+          location: 'Meow Town',
+          date: 'January 6, 2025',
+          time: '12:00',
+          petsAllowed: true,
+          organization: 'Kat Laydee'
+        },
+        {
+          id: 4582797,
+          category: 'food',
+          title: 'Community Gardening',
+          description: 'Join us as we tend to the community edible plants.',
+          location: 'Flora City',
+          date: 'March 14, 2022',
+          time: '10:00',
+          petsAllowed: true,
+          organizer: 'Fern Pollin'
+        },
+        {
+          id: 8419988,
+          category: 'sustainability',
+          title: 'Beach Cleanup',
+          description: 'Help pick up trash along the shore.',
+          location: 'Playa Del Carmen',
+          date: 'July 22, 2022',
+          time: '11:00',
+          petsAllowed: false,
+          organizer: 'Carey Wales'
+        }]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+</style>
